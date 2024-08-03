607760e5ead7c3448984bf9dd01531df84365f5c
Risyad02 <risyadkamarullah2019@gmail.com>
2024-08-03 09:45:26 +0700

    add Documentation API

--- MiniProject2/Models/OrderRequest.cs
+++ MiniProject2/Models/OrderRequest.cs
@@ -2,6 +2,29 @@
 {
     public class OrderRequest
     {
+        /// <summary>
+        /// New Class for place order 
+        /// </summary>
+
+        /// <remarks>
+        /// Get the data [FromBody] 
+        ///
+        /// CustomerId, MenuId, Note
+        ///  
+        ///  NOTE: 
+        ///  
+        /// Sample request:
+        ///
+        ///     POST api/Order/
+        ///     {
+        ///        "customerId": 0,
+        ///            "menuId": 
+        ///                 [0],
+        ///        "note": "string""userId": null,
+        ///     }
+        /// </remarks>
+        /// <param name="request"></param>
+        /// <returns> This endpoint returns a list of Accounts.</returns>
         public int CustomerId { get; set; }
         public List<int> MenuId { get; set; }
         public string Note { get; set; }
